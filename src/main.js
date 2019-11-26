864fa22965f1b01013bedb461406fdee9a8567a6
Regina Orozco <regina.orozco91@gmail.com>
2019-11-26 09:13:32 -0600

    buscador alineado a la derecha, botón ver más para abrir modal, modal con información de personaje

--- src/main.js
+++ src/main.js
@@ -27,10 +27,10 @@ const funcionMostrar = (arregloPersonajes) => {
          personajeGenderSpan.textContent = harry.gender;
 
          const personajeAncestrySpan = todosPersonajesTemplate.content.querySelector(".ancestrySpan");
-         personajeAncestrySpan.textContent = harry.ancestry;        
+         personajeAncestrySpan.textContent = harry.ancestry;
 
          const personajePatronusSpan = todosPersonajesTemplate.content.querySelector(".patronusSpan");
-         personajePatronusSpan.textContent = harry.patronus; 
+         personajePatronusSpan.textContent = harry.patronus;
 
          const personajeWoodSpan = todosPersonajesTemplate.content.querySelector(".woodSpan");
          personajeWoodSpan.textContent = harry.wand.wood;
@@ -47,18 +47,21 @@ const funcionMostrar = (arregloPersonajes) => {
          const personajeHairColourSpan = todosPersonajesTemplate.content.querySelector(".hairColourSpan");
          personajeHairColourSpan.textContent = harry.hairColour;
 
+         const personajeStatusSpan = todosPersonajesTemplate.content.querySelector(".statusSpan");
+         personajeStatusSpan.textContent = harry.alive ? "Alive" : "Dead";
+
          const personajeActorSpan = todosPersonajesTemplate.content.querySelector(".actorSpan");
          personajeActorSpan.textContent = harry.actor;
 
          const contenidoTemplate = todosPersonajesTemplate.content;
          const clonePersonajes = document.importNode(contenidoTemplate, true);
          divsContenedor.appendChild(clonePersonajes);
+
     });
 };
 
 
 funcionMostrar(potterObj);
-
 /*
 //const genderFilter = potterObj.filter(d => d.gender === "female");
 //console.log(genderFilter);
@@ -69,9 +72,9 @@ funcionMostrar(resultCasa);
 console.log(resultCasa);
 */
 
-const ejecutarFiltro = () => {
-    //leer el value del input de la lista de casas!!
-    const resultCasa = filtrarPorCasa('Slytherin', potterObj);
+const ejecutarFiltro = () => { //esta es la función para filtrar con elementos del html
+  let filtroCasas = document.getElementById("tiposDeCasa").value;
+    const resultCasa = filtrarPorCasa(filtroCasas, potterObj);
     funcionMostrar(resultCasa);
 };
 
