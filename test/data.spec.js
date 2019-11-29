657b24a0f8eb2578259fe0f1272dda40be7dc3db
Mirey91 <mirey.morales91@gmail.com>
2019-11-29 13:05:39 -0600

    test

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,13 +1,27 @@
 // importamos la función `example`
-import { example } from "../src/data";
+// import  data from '../src/data';
+import filtrarPorCasa from '../src/data';
+//import potterObj from'../src/data/potter/potter.js';
+import {potterObj} from'../src/data/potter/potter.js';
+const harryPotter = potterObj;
 
-describe('example', () => {
-
-  it('debería ser una función', () => {
-    expect(typeof example).toBe('function');
-  });
-
-  describe('example', () => {
+// describe("data", () => {
+  // it('debería ser una objeto', () => {
+  //   expect(typeof data).toBe('object');
+  // })
+  
+  describe("filtrarPorCasa", () => {
     // escribe aquí tu test
+    it("filtrar por casa debe ser una funcion", () => {
+        expect(typeof filtrarPorCasa).toBe("function");
+    });
+
+    it("deberia retornar 'Harry Potter' en la primera pocision de la casa de Gryffindor", () => {
+      expect(filtrarPorCasa("Gryffindor", harryPotter)[0])
+      .toHaveProperty("house", "Gryffindor");
+    });
+
+    let aux = filtrarPorCasa("Gryffindor", harryPotter)[0];
+    console.log(aux.house);
   });
-});
+// });
